377d5caf9922de40154970f288b30074465c03d2
CesarLeiva26 <cesaremanuelleivajuro@gmail.com>
2023-06-23 13:12:43 -0500

    initial commit

---
+++ src/main/java/cibertec/edu/pe/DAWI_CL3_GRUPO_6/DawiCl3Grupo6Application.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DAWI_CL3_GRUPO_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl3Grupo6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl3Grupo6Application.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/DAWI_CL3_GRUPO_6/DawiCl3Grupo6ApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DAWI_CL3_GRUPO_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl3Grupo6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
