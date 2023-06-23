15b1ff0a69ecb5678c2f750519f3234df792c747
CesarLeiva26 <cesaremanuelleivajuro@gmail.com>
2023-06-23 13:18:52 -0500

    first commit

---
+++ src/main/java/cibertec/edu/pe/DAWI_CL3_GRUPO_6/model/bd/Usuario.java
@@ -0,0 +1,37 @@
+package cibertec.edu.pe.DAWI_CL3_GRUPO_6.model.bd;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+ 
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+ 
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name ="usuario")
+public class Usuario {
+
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer codigo;
+	@Column(name = "nombre")
+	private String nombre;
+	@Column(name = "apellido")
+	private String apellido;
+	@Column(name = "email")
+	private String email;
+	@Column(name = "usuario")
+	private String usuario;
+	@Column(name = "password")
+	private String password;
+}
